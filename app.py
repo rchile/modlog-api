52a940d3d4b7d9ea24b90148588d3eccc464d48d
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2019-03-06 17:39:24 -0300

    CORS

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, redirect
+from flask_cors import CORS
 from common import *
 
 reddit = get_reddit_instance()
 sub = reddit.subreddit('chile')
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
