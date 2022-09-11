c0a09630f6958c13dd515c2e49cf7b2e691a3ffd
Mak <contacto@jgutierrez.cl>
2022-09-10 22:36:52 -0400

    file perms and hidden actions

--- settings.py
+++ settings.py
@@ -6,7 +6,5 @@ load_dotenv()
 
 MONGODB_URL = os.getenv('MONGODB_URL')
 DEFAULT_PAGE_LIMIT = try_int(os.getenv('DEFAULT_PAGE_LIMIT'), 20)
-CORS_ORIGINS = os.getenv('CORS_ORIGINS', '').split(',')
-
-if CORS_ORIGINS == ['']:
-    CORS_ORIGINS = []
+CORS_ORIGINS = [x for x in os.getenv('CORS_ORIGINS', '').split(',') if x]
+HIDDEN_ACTIONS = [x for x in os.getenv('HIDDEN_ACTIONS', '').split(',') if x]
